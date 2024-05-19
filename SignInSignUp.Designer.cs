faf5eb93d5eae6fc4d97dac50c8863d4d2c5ffcc
Ahmet Ozbay <ahmet.ozbay@outlook.com.tr>
2024-05-19 22:20:57 +0300

    Visual fixes

--- SignInSignUp.Designer.cs
+++ SignInSignUp.Designer.cs
@@ -71,13 +71,13 @@ namespace FinalProject
             this.lbl_signup = new System.Windows.Forms.Label();
             this.bg_flowers = new System.Windows.Forms.Label();
             this.pnl_reset_password = new System.Windows.Forms.Panel();
+            this.btn_reset_password = new System.Windows.Forms.Button();
             this.lbl_sign_in_reset = new System.Windows.Forms.LinkLabel();
             this.lbl_reset_pass_info = new System.Windows.Forms.Label();
             this.pnl_email_bg_reset_pass = new System.Windows.Forms.Panel();
             this.txt_email_reset_pass = new System.Windows.Forms.TextBox();
             this.lbl_email_reset_pass = new System.Windows.Forms.Label();
             this.lbl_reset_pass_title = new System.Windows.Forms.Label();
-            this.btn_reset_password = new System.Windows.Forms.Button();
             this.pnl_header.SuspendLayout();
             this.pnl_signin.SuspendLayout();
             this.panel_password.SuspendLayout();
@@ -450,14 +450,25 @@ namespace FinalProject
             // pnl_reset_password
             // 
             this.pnl_reset_password.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_reset_password.Controls.Add(this.lbl_reset_pass_title);
             this.pnl_reset_password.Controls.Add(this.btn_reset_password);
             this.pnl_reset_password.Controls.Add(this.lbl_sign_in_reset);
             this.pnl_reset_password.Controls.Add(this.lbl_reset_pass_info);
             this.pnl_reset_password.Controls.Add(this.pnl_email_bg_reset_pass);
             this.pnl_reset_password.Controls.Add(this.lbl_email_reset_pass);
-            this.pnl_reset_password.Controls.Add(this.lbl_reset_pass_title);
             resources.ApplyResources(this.pnl_reset_password, "pnl_reset_password");
             this.pnl_reset_password.Name = "pnl_reset_password";
+            // 
+            // btn_reset_password
+            // 
+            this.btn_reset_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(84)))), ((int)(((byte)(94)))));
+            this.btn_reset_password.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_reset_password.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.btn_reset_password, "btn_reset_password");
+            this.btn_reset_password.ForeColor = System.Drawing.Color.White;
+            this.btn_reset_password.Name = "btn_reset_password";
+            this.btn_reset_password.UseVisualStyleBackColor = true;
+            this.btn_reset_password.Click += new System.EventHandler(this.btn_reset_password_Click);
             // 
             // lbl_sign_in_reset
             // 
@@ -506,25 +517,14 @@ namespace FinalProject
             this.lbl_reset_pass_title.Image = global::FinalProject.Properties.Resources.icon_forgot_password;
             this.lbl_reset_pass_title.Name = "lbl_reset_pass_title";
             // 
-            // btn_reset_password
-            // 
-            this.btn_reset_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(84)))), ((int)(((byte)(94)))));
-            this.btn_reset_password.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.btn_reset_password.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            resources.ApplyResources(this.btn_reset_password, "btn_reset_password");
-            this.btn_reset_password.ForeColor = System.Drawing.Color.White;
-            this.btn_reset_password.Name = "btn_reset_password";
-            this.btn_reset_password.UseVisualStyleBackColor = true;
-            this.btn_reset_password.Click += new System.EventHandler(this.btn_reset_password_Click);
-            // 
             // Form_SignIn_SignUp
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackgroundImage = global::FinalProject.Properties.Resources.bg_gradient;
+            this.Controls.Add(this.pnl_reset_password);
             this.Controls.Add(this.pnl_signin);
             this.Controls.Add(this.pnl_signup);
-            this.Controls.Add(this.pnl_reset_password);
             this.Controls.Add(this.pnl_header);
             this.Controls.Add(this.bg_flowers);
             this.DoubleBuffered = true;
